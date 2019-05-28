ed9af88f82b26c2857774ced2fabe088490dc25b
niuhaideng <niuhaideng@163.com>
2019-05-28 19:29:02 +0800

    point test

--- point_test.cpp
+++ point_test.cpp
@@ -6,5 +6,12 @@
 using namespace std;
 
 int main(){
+    int  var1;
+    char var2[10];
 
+    cout << "var1 变量的地址： ";
+    cout << &var1 << endl;
+
+    cout << "var2 变量的地址： ";
+    cout << &var2 << endl;
 }
