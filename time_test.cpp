c3aa12a7ad76f735b92ec7345b95f337e8887403
niuhaideng <niuhaideng@163.com>
2019-05-28 20:11:21 +0800

    time

--- time_test.cpp
+++ time_test.cpp
@@ -2,12 +2,22 @@
 // Created by ami on 2019/5/28.
 //
 #include <iostream>
-#include <cstring>
-
+#include <ctime>
 using namespace std;
 
 int main() {
+    // 基于当前系统的当前日期/时间
+    time_t now = time(0);
 
+    // 把 now 转换为字符串形式
+    char* dt = ctime(&now);
+
+    cout << "本地日期和时间：" << dt << endl;
+
+    // 把 now 转换为 tm 结构
+    tm *gmtm = gmtime(&now);
+    dt = asctime(gmtm);
+    cout << "UTC 日期和时间："<< dt << endl;
     return 0;
 
 }
